8d1f14c11d9881139cbe32903a3f4f8af639094e
DimaAmega <dima.amega1@gmail.com>
2023-11-06 14:46:04 +0400

    wip

--- ui/v2/service/tests/simple.spec.ts
+++ ui/v2/service/tests/simple.spec.ts
@@ -6,41 +6,7 @@ test('Has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Evidently/)
 })
 
-test('Altering project title and description', async ({ page }) => {
-  await page.goto('/')
-
-  await expect(page.getByText('Project List')).toBeVisible()
-
-  await page.getByRole('link', { name: 'Demo project - Reviews' }).hover()
-
-  // edit button
-  await page
-    .locator('#root')
-    .filter({
-      hasText:
-        'A toy demo project using E-commerce Reviews dataset. Text and tabular data, classification.'
-    })
-    .getByRole('button')
-    .click()
-
-  await page.locator('input[name="name"]').fill('Reviews new title')
-  await page.locator('input[name="description"]').fill('Reviews new description')
-
-  await page.getByText('Save').click()
-
-  await page.waitForLoadState('domcontentloaded')
-
-  await expect(page.getByText('Reviews new description')).toBeVisible()
-  await expect(page.getByText('Reviews new title')).toBeVisible()
-
-  await page.getByRole('link', { name: 'Reviews new' }).click()
-
-  await page.waitForLoadState('domcontentloaded')
-
-  await expect(page.getByText('Dashboard')).toBeVisible()
-})
-
-test('Navigation test', async ({ page }) => {
+test('Navigation test (aka DFS by links...)', async ({ page }) => {
   await page.goto('/')
   await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
   await page.getByText('Bike Rental Demand Forecast').click()
@@ -66,7 +32,7 @@ test('Navigation test', async ({ page }) => {
   await expect(page.getByText('Drift per Column', { exact: true }).first()).toBeVisible()
 })
 
-test('Download reports and test suites test', async ({ page }) => {
+test('Download reports and test suites', async ({ page }) => {
   await page.goto('/')
   await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
 
@@ -90,4 +56,43 @@ test('Download reports and test suites test', async ({ page }) => {
       expect(await download.failure()).toBeNull()
     }
   }
+})
+
+test('We expect to see at least 3 plotly graphs', async ({ page }) => {
+  await page.goto('/')
+  await page.getByRole('link', { name: 'Demo project - Bikes' }).click()
+  for (let index = 0; index < 3; index++) {
+    await expect(page.locator('.js-plotly-plot').nth(index)).toBeVisible()
+  }
+})
+
+test('Altering project title and description', async ({ page }) => {
+  await page.goto('/')
+
+  await expect(page.getByText('Project List')).toBeVisible()
+
+  await page.getByRole('link', { name: 'Demo project - Reviews' }).hover()
+
+  await page
+    .getByText(
+      'Demo project - ReviewsA toy demo project using E-commerce Reviews dataset. Text and tabular data, classification.'
+    )
+    .getByTestId('EditIcon')
+    .click()
+
+  await page.locator('input[name="name"]').fill('Reviews new title')
+  await page.locator('input[name="description"]').fill('Reviews new description')
+
+  await page.getByText('Save').click()
+
+  await page.waitForLoadState('domcontentloaded')
+
+  await expect(page.getByText('Reviews new description')).toBeVisible()
+  await expect(page.getByText('Reviews new title')).toBeVisible()
+
+  await page.getByRole('link', { name: 'Reviews new' }).click()
+
+  await page.waitForLoadState('domcontentloaded')
+
+  await expect(page.getByText('Dashboard')).toBeVisible()
 })
